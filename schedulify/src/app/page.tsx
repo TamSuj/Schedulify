4ecb10899186407dff3391ba1354f2775f18f3b2
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-08-16 14:11:28 -0700

    clean

--- schedulify/src/app/page.tsx
+++ schedulify/src/app/page.tsx
@@ -1,30 +1,12 @@
 "use client";
 import Header from "./header";
-import Features from "./features";
-import Footer from "./footer";
-import Workflow from "./workflow";
-import Link from "next/link";
 import Calendar from "@/app/calendar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-white text-black">
       <Header />
-      <section className="flex-grow flex flex-col items-center justify-center px-6 py-12 mx-auto max-w-4xl">
-        <div className="grid gap-10 lg:grid-cols-2 w-full">
-          <Features />
-          <Workflow />
-        </div>
-        <div className="mt-8">
-          <Link href="/interaction">
-            <button className="px-6 py-3 bg-gradient-to-r from-green-400 to-teal-500 text-white font-semibold rounded-lg shadow-lg transition-transform transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-500">
-              Interact with AI Scheduler
-            </button>
-          </Link>
-        </div>
-      </section>
       <Calendar />
-      <Footer />
     </main>
   );
 }
